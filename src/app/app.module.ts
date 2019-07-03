52c44c770724b9f10a597d945d1405bfdcc8d1c3
Daniel Ostrovsky <danduh@gmail.com>
2019-07-03 06:53:03 +0300

    styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {FormsModule} from '@angular/forms';
 import {ApiService} from './services/api.service';
 import {TodoSummaryComponent} from './pages/components/todo-summary/todo-summary.component';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -26,6 +27,7 @@ import {HttpClientModule} from '@angular/common/http';
         AppRoutingModule,
         FormsModule,
         HttpClientModule,
+        BrowserAnimationsModule,
     ],
     providers: [
         ApiService
